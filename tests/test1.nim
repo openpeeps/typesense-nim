a6e8f7cae79a696dd2921172aa510be4d8185c98
George Lemon <georgelemon@protonmail.com>
2023-10-13 03:51:25 +0300

    fix
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- tests/test1.nim
+++ tests/test1.nim
@@ -5,7 +5,7 @@ let TSKey = getEnv("TYPESENSE_KEY").strip()
 
 test "can init":
   var ts = newClient(
-    address = "0.0.0.0"
-    port = Port(8108)
+    address = "0.0.0.0",
+    port = Port(8108),
     apiKey = TSKey
   )

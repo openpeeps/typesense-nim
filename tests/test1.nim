b1e2659fe1b78a12835a261080ea7bca1338ded9
George Lemon <georgelemon@protonmail.com>
2023-10-13 19:30:18 +0300

    update tests
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,11 +1,34 @@
-import std/[unittest, envvars, strutils]
+import std/[unittest, envvars, strutils, asyncdispatch]
 import typesense
 
-let TSKey = getEnv("TYPESENSE_KEY").strip()
+let
+  rootkey = getEnv("TYPESENSE_KEY").strip()
+  address = "0.0.0.0"
+
+template newTypesenseClient() {.dirty.} =
+  var ts = newClient(address, Port(8108), rootkey)
 
 test "can init":
-  var ts = newClient(
-    address = "0.0.0.0",
-    port = Port(8108),
-    apiKey = TSKey
-  )
+  newTypesenseClient()
+  assert waitFor(ts.health())
+  ts.close()
+
+test "/keys POST":
+  newTypesenseClient()
+  let key = waitFor ts.keys.create("Some key")
+  assert key.getActions == {tsAnyActions}
+
+test "/keys GET":
+  newTypesenseClient()
+  let allkeys = waitFor ts.keys.retrieve 
+  assert allkeys.len != 0
+
+test "/keys DELETE":
+  newTypesenseClient()
+  waitFor ts.keys.deleteAll()
+  let allkeys = waitFor ts.keys.retrieve
+  assert allkeys.len == 0
+
+test "/collections":
+  newTypesenseClient()
+  let collections = waitFor ts.collections.retrieve()

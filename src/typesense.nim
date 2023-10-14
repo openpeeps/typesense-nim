7a5db6f6f4e3694edb8a34dac5080e0e32877044
George Lemon <georgelemon@protonmail.com>
2023-10-15 02:57:51 +0300

    export `documents` module
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/typesense.nim
+++ src/typesense.nim
@@ -4,5 +4,5 @@
 #   (c) 2023 George Lemon | MIT License
 #   Made by Humans from OpenPeeps
 
-import typesense/[client, keys, collections, aliases]
-export client, keys, collections
+import typesense/[client, keys, collections, documents, aliases]
+export client, keys, collections, documents
